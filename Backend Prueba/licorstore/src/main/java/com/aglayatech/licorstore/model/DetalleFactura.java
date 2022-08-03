c40d65918f9b3c2c0259cdae6d74f88310b548de
Eduardo Fuentes <ramieduar@gmail.com>
2022-08-03 09:17:43 -0600

    Se corrigio problema de produccion grave

--- Backend Prueba/licorstore/src/main/java/com/aglayatech/licorstore/model/DetalleFactura.java
+++ Backend Prueba/licorstore/src/main/java/com/aglayatech/licorstore/model/DetalleFactura.java
@@ -28,7 +28,7 @@ public class DetalleFactura implements Serializable {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "id_producto")
-	@JsonIgnoreProperties({"tipoProducto", "marcaProducto", "movimientos", "hibernateLazyInitializer", "handler" })
+	@JsonIgnoreProperties({ "movimientos", "hibernateLazyInitializer", "handler" })
 	private Producto producto;
 
 	public DetalleFactura() {
